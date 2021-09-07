72093d2da065a176218cd499bbf629dc4e5f5f4f
danny <danny756883@gmail.com>
2021-09-08 05:59:52 +0900

    solve boj 18870

---
+++ BOJ/boj18870.py
@@ -0,0 +1,22 @@
+N = int(input())
+inp = list(map(int, input().split()))
+sx = list()
+out = []
+for n, xn in enumerate(inp):
+    sx.append([n, xn])
+sx.sort(key = lambda k: k[1])
+count = 0
+press = [0]
+for i in range(1, N):
+    if sx[i-1][1] != sx[i][1]:
+        count += 1
+    press.append(count)
+# print(sx)
+# print(press)
+for i in range(0, N):
+    sx[i][1] = press[i]
+# print(sx)
+sx.sort(key = lambda k: k[0])
+for i in range(0, N):
+    out.append(str(sx[i][1]))
+print(' '.join(out))

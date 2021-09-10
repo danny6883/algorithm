3d1f9fac9e159652984388e6871649e5c9cf6982
danny <danny756883@gmail.com>
2021-09-10 22:27:30 +0900

    solve boj 15650

---
+++ BOJ/boj15650.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+out = []
+
+def dfs():
+    if len(out) == m:
+        print(' '.join(map(str, out)))
+        return
+    
+    for i in range(1, n+1):
+        if len(out) == 0 or i > out[-1]:
+            out.append(i)
+            dfs()
+            out.pop()
+dfs()

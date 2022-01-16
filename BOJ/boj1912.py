815cd62c461abec25620ce2dab5dc255c1069165
danny <danny756883@gmail.com>
2022-01-17 00:06:50 +0900

    solve boj 1912

--- BOJ/boj1912.py
+++ BOJ/boj1912.py
@@ -2,6 +2,7 @@ n = int(input())
 seq = list(map(int, input().split()))
 
 total = []
-total.append(seq[0])
-
-
+total.append([seq[0],seq[0]])
+for i in range(1,n):
+    total.append([max(total[i-1]),max(total[i-1][1]+seq[i],seq[i])])
+print(max(total[-1]))
